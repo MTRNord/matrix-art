75a3404172a4d56f5b16c691661347113fe321e7
MTRNord <mtrnord1@gmail.com>
2021-12-30 22:49:07 +0100

    Add missing opacity

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -172,7 +172,7 @@ export default class Home extends Component<Props, State>{
                         </div>
                     </header>
                     <main className='lg:pt-[108px] pt-[216px] z-0'>
-                        <div className='z-[100] sticky lg:top-[108px] top-[216px] bg-[#fefefe] dark:bg-[#14181E]'>
+                        <div className='z-[100] sticky lg:top-[108px] top-[216px] bg-[#fefefe]/[.95] dark:bg-[#14181E]/[.95]'>
                             <div className='h-[72px] px-10 w-full relative grid grid-cols-[1fr_auto_1fr] items-center' id='section-grid'>
                                 <h1 className='text-xl text-gray-900 dark:text-gray-200 font-bold'>Home</h1>
                             </div>
