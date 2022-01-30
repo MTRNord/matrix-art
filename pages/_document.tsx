07bba607fc34ac863be7dcd679f3cae1de329ed8
MTRNord <mtrnord1@gmail.com>
2022-01-30 18:09:02 +0100

    Use local fonts instead of google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,13 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+                    <link
+                        rel="preload"
+                        href="/fonts/Inter-VariableFont_slnt,wght.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin="anonymous"
+                    />
                 </Head>
                 <body>
                     <Main />
