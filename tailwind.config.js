42662633c64848af2b5fd3a29265ecfad36f1bd5
MTRNord <mtrnord1@gmail.com>
2022-03-30 22:23:31 +0200

    Cleanup and eslint and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}",],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
