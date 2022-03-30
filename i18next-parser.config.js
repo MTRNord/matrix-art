73c7fa569aa3d302fdb94230a65e5ac934f2e030
MTRNord <mtrnord1@gmail.com>
2022-03-30 21:12:49 +0200

    chore: Reset to vite template

--- i18next-parser.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    locales: ['en'],
-    defaultNamespace: 'common',
-    output: 'public/locales/$LOCALE/$NAMESPACE.json',
-    indentation: 4,
-    namespaceSeparator: false,
-    keySeparator: false
-}
