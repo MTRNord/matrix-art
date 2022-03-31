95fea732d3e65cfd0152c7d6566b71e29b2e624e
MTRNord <mtrnord1@gmail.com>
2022-03-31 02:31:13 +0200

    Setup compat

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
-import { Component } from "preact";
+import { PureComponent } from 'preact/compat';
 import { MatrixClient } from "./matrix/client";
 
-export class App extends Component {
+export class App extends PureComponent {
   async componentDidMount() {
     const client = await MatrixClient.new();
     await client.start();
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
-})
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react/jsx-runtime': 'preact/jsx-runtime'
+    }
+  }
+});
