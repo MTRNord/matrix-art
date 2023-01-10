4f860d87f37bcc4c432e804f5cd562302b7e4577
MTRNord <MTRNord@users.noreply.github.com>
2023-01-10 09:16:18 +0000

    Deploying to gh-pages from @ MTRNord/matrix-art@a93549c550af8df50a4cdcd433055038a224bc98 🚀

--- assets/Profile-0a0cba7f.js
+++
@@ -1,2 +0,0 @@
-import{p as e,j as s}from"./vendor-50c33f04.js";import{H as a}from"./index-f811434d.js";function i(){return e(),s.jsxs("div",{className:"flex flex-col",children:[s.jsx("header",{children:s.jsx(a,{})}),s.jsx("main",{className:"m-12 mt-6"})]})}export{i as default};
-//# sourceMappingURL=Profile-0a0cba7f.js.map
